56036502c90ad0929935e73c4dd4f8f6610f5a76
Anaya_Abro <abroayaan8@gmail.com>
2026-04-07 17:52:00 +0500

    Add files via upload

---
+++ lucky_number.py
@@ -0,0 +1,38 @@
+import random
+
+print("💫welcome to lucky number game💫")
+while True:
+ computer = random.randint(1,5)
+ user_guess = int(input('Enter your lucky guess number🤞🍀:(1 to 5)'))
+
+ if user_guess == computer:
+    print(f'You win 🎉 | you chose {user_guess}, computer chose {computer}')
+    
+
+ else:
+    print(f'you lose 😟 | you chose {user_guess}, computer chose {computer}|')
+    continue
+
+ play_again = input('Do you want to play again(Yes/No) ').lower()
+ if play_again == 'no':
+   print("Thank you for playing! ")
+   break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
