910d646ab1c48c6b400bae3979c43111bd3ca872
Anaya_Abro <abroayaan8@gmail.com>
2026-04-07 17:52:35 +0500

    Update lucky_number.py

--- lucky_number.py
+++ lucky_number.py
@@ -6,11 +6,11 @@ while True:
  user_guess = int(input('Enter your lucky guess number🤞🍀:(1 to 5)'))
 
  if user_guess == computer:
-    print(f'You win 🎉 | you chose {user_guess}, computer chose {computer}')
+    print(f'You win 🎉 | You chose {user_guess}, computer chose {computer}')
     
 
  else:
-    print(f'you lose 😟 | you chose {user_guess}, computer chose {computer}|')
+    print(f'you lose 😟 | You chose {user_guess}, computer chose {computer}|')
     continue
 
  play_again = input('Do you want to play again(Yes/No) ').lower()
